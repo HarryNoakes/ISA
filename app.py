645e94590e7123e689707b660f84de1ef2630482
HarryNoakes <noaksy1997@hotmail.com>
2020-03-20 14:06:06 +0000

    Made the first and second page for the site.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template,redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/index.html')
+def root():
+    return redirect('/')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
