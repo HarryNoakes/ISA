7bf77edb8336b56a2dc273bfc7a21a205eaf9dd7
HarryNoakes <noaksy1997@hotmail.com>
2020-03-23 17:04:59 +0000

    Login Page added with login logic, handles log in requests. Nav bar updated.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template, flash, redirect, url_for
+from app.forms import LoginForm
+from config import Config
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
 
 
 @app.route('/')
@@ -19,13 +24,22 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/index.html')
+@app.route('/index')
 def root():
     return redirect('/')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == '__main__':
     app.run()
