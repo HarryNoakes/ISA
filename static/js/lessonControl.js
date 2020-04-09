0d77c9e970252aaa2af0f35a0b3031300c8c536a
HarryNoakes <noaksy1997@hotmail.com>
2020-04-09 15:35:09 +0100

    Another lesson button added, comments regarding how to handle which lesson when a button is pressed have been added.

--- static/js/lessonControl.js
+++ static/js/lessonControl.js
@@ -1,3 +1,11 @@
 function nextLesson() {
 document.getElementById("lessonText").innerHTML ="hello welcome to lesson 1";
 }
+
+function prevLesson() {
+document.getElementById("lessonText").innerHTML ="test";
+}
+
+/*for loop counting each button press with i, set whatever i is
+to a lesson text with a limit of 5 counts.
+ */
