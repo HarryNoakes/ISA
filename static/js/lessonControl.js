087c4ac29cb3ba1d6f9d26c7a0a92229633f5809
HarryNoakes <noaksy1997@hotmail.com>
2020-04-10 13:56:30 +0100

    Lesson functionality added, javascript to switch between each lesson using a click counter to determine each lesson.

--- static/js/lessonControl.js
+++ static/js/lessonControl.js
@@ -1,9 +1,50 @@
+var click = 0;
 function nextLesson() {
-document.getElementById("lessonText").innerHTML ="hello welcome to lesson 1";
+click ++;
+if (click === 1) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 1."
+}
+else if(click === 2) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 2."
+}
+else if(click === 3) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 3."
+}
+else if(click === 4) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 4."
+}
+else if(click === 5) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 5."
+}
+else if(click === 6) {
+    click = 5;
+}
+
 }
 
 function prevLesson() {
-document.getElementById("lessonText").innerHTML ="test";
+click --;
+if (click === 1) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 1."
+}
+else if(click === 2) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 2."
+}
+else if(click === 3) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 3."
+}
+else if(click === 4) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 4."
+}
+else if(click === 5) {
+    document.getElementById("lessonText").innerHTML ="Welcome to lesson 5."
+}
+else if (click === -1) {
+    click = 0;
+}
+else {
+    document.getElementById("lessonText").innerHTML ="Welcome to you lesson introduction, in this space a lesson will appear giving you instructions on what you must do. When are you are ready to get started, please click next lesson."
+    }
 }
 
 /*for loop counting each button press with i, set whatever i is
