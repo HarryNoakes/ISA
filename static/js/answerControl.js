167f226ada736003601b7e2e1f3a381dab9c08db
HarryNoakes <noaksy1997@hotmail.com>
2020-04-16 15:08:49 +0100

    Training page completed, answer section hides buttons and gives you the answer to what you chose.

--- static/js/answerControl.js
+++ static/js/answerControl.js
@@ -2,50 +2,44 @@ function checkAnswer(emailid, answer) {
     // Replaces all non-digts with whitespace.
     var newEmailId = emailid.replace( /^\D+/g, '');
     var results =
-            '{"testemail1":1,"testemail2":1,"testemail3":1,"testemail4":1,"testemail5":0,"testemail6":0,"testemail7":0,"testemail8":0,"testemail9":0}';
+            '{"testemail1":0,"testemail2":0,"testemail3":0,"testemail4":0,"testemail5":1,"testemail6":0,"testemail7":0,"testemail8":0,"testemail9":1}';
     var resultsParsed = JSON.parse(results);
     var correctAnswer = resultsParsed["testemail"+newEmailId];
 
     if (answer === correctAnswer){
-        //Show user that they are correct I get.
-        var paragraph = document.getElementById("answerConfirm");
-        var text = document.createTextNode("You are correct!" + returnAnswer(correctAnswer));
+        //Shows user that they are correct.
+        var paragraph = document.getElementById("answerConfirm" + newEmailId);
+        var text = document.createTextNode("You are correct! " + returnAnswer(correctAnswer));
         paragraph.appendChild(text);
-
     }
     else if (answer != correctAnswer){
-        //Tell the user that they are dumb
-        var paragraph = document.getElementById("answerConfirm");
+        //Tells the user that they are incorrect.
+        var paragraph = document.getElementById("answerConfirm" + newEmailId);
         var text = document.createTextNode("Incorrect answer! " + returnAnswer(correctAnswer));
         paragraph.appendChild(text);
     }
     else{
-        //something is not right, handle this expection
+        //something is not right, handle this exception
+
     }
-    document.getElementById("answerbtn" + emailid).hidden
+    console.log(emailid);
+    buttons = document.getElementById("answerButtons" + newEmailId);
+    childBtns = document.childNodes;
+    for(i=0;i<buttons.childNodes.length;i++){
+        var child = buttons.childNodes[i];
+        //child.remove();
+    }
+    //This is really gross and I hate myself for it.
+    document.getElementById(emailid).remove();
+    document.getElementById(emailid).remove();
 }
 
 function returnAnswer(correctAnswer){
     if (correctAnswer === 0 ){
         return "Email is a not a phishing email"
+
     }
     else if (correctAnswer === 1){
         return "Email is a phishing email"
     }
 }
-
-
-
-function correctAns(id) {
-    var emailId = document.getElementById(id).nextElementSibling;
-    if (emailId === 1) {
-        document.getElementById("answerConfirm").innerHTML = "Email one was not a phishing email, correct!"
-    }
-}
-function incorrectAns(id) {
-    var emailId = document.getElementByImicd(id).nextElementSibling;
-
-    if (emailId === 0) {
-        document.getElementById("answerConfirm").innerHTML = "Incorrect answer, email one is not a phishing email!"
-    }
-    }
--- webapp/routes.py
+++ webapp/routes.py
@@ -57,11 +57,13 @@ def training():
                     "iframeid":"email"+str(x),
                     "src":"email/testemail"+str(x),
                     "iframename":"iframe"+str(x)
-                    } for x in range(1, 9)]
+                    } for x in range(1, 10)]
     #replicate email slots with answer slots?
     answer_slots = [{"slot": "slot" + str(y),
+                     "answerButtons":"answerButtons" + str(y),
                      "answerid":"answerBtn:"+str(y),
-                     "name":"Is Email "+str(y)+" a phishing email?" } for y in range(1, 9)]
+                     "answerConfirm":"answerConfirm"+str(y),
+                     "name":"Is Email "+str(y)+" a phishing email?" } for y in range(1, 10)]
     return render_template('training.html', emails=email_slots, answers=answer_slots)
 
 
